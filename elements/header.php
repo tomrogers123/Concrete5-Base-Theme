3fb7f3fbce0d5dd47dbb7a85e1a2b0edf53b3131
tomrogers123 <tom@therogersfamilyonline.com>
2012-07-29 12:42:42 +0100

    Fixed issues with missing clossing DIV element

--- elements/header.php
+++ elements/header.php
@@ -1,6 +1,6 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
 <!DOCTYPE html>
-<html lang="<?php echo LANGUAGE?>" >
+<html lang="<?php echo LANGUAGE ?>">
 <head>
   
   <?php  Loader::element('header_required'); ?>
@@ -9,39 +9,27 @@
 </head>
 <body>
 
-<div id="page">
-	<header>
-		
-		<?php  if ($c->isEditMode()) { ?>
-		<div style="min-height: 80px">
-		<?php  } ?>
-
-		<?php $ah = new Area('Header');	$ah->display($c); ?>	
-		
-		<nav>
-			<?php 
-			$a = new Area('Header Nav');
-			$a->display($c);
-			?>
-		</nav>
-		
-		<h1>
-			<a href="<?php   echo DIR_REL?>/"><?php   
-				$block = Block::getByName('My_Site_Name');  
-				if( $block && $block->bID ) $block->display();   
-				else echo SITE;
-			?></a>
-		</h1>
-
-		<?php 
-		// we use the "is edit mode" check because, in edit mode, the bottom of the area overlaps the item below it, because
-		// we're using absolute positioning. So in edit mode we add a bit of space so everything looks nice.
-		?>
-
-
-		<?php  if ($c->isEditMode()) { ?>
-		</div>
-		<?php  } ?>
+	<div id="page">
+		<header>
 			
-		</header>
-	</div>			
+			<?php  if ($c->isEditMode()) { ?>
+		    	<div style="min-height: 80px">
+			<?php  } ?>
+
+			<?php $ah = new Area('Header');	$ah->display($c); ?>	
+			
+			<nav><?php $a = new Area('Header Nav'); $a->display($c); ?></nav>
+			
+			<h1>
+				<a href="<?php echo DIR_REL?>/"><?php   
+					$block = Block::getByName('My_Site_Name');  
+					if( $block && $block->bID ) $block->display();   
+					else echo SITE;
+				?></a>
+			</h1>
+
+			<?php  if ($c->isEditMode()) { ?>
+			  </div>
+			<?php  } ?>
+				
+		</header>			
